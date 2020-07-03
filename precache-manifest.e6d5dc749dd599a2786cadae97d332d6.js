c9902d077cc87f0b7aa173ad0b68f204a3901d91
ashniparikh <ashniparikh94@gmail.com>
2020-07-02 18:26:59 -0700

    Updates

---
+++ precache-manifest.e6d5dc749dd599a2786cadae97d332d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af0a4852aac2e4c90b9d7c5f03403299",
+    "url": "/RestaurantData/index.html"
+  },
+  {
+    "revision": "95f2116ba13510ac614d",
+    "url": "/RestaurantData/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "6a4b79d9bf5d001dc378",
+    "url": "/RestaurantData/static/js/2.a5950151.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/RestaurantData/static/js/2.a5950151.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "95f2116ba13510ac614d",
+    "url": "/RestaurantData/static/js/main.fb292a23.chunk.js"
+  },
+  {
+    "revision": "37a6c7352e3270653a36",
+    "url": "/RestaurantData/static/js/runtime-main.b499b622.js"
+  }
+]);
